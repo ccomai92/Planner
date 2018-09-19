b801f3e6d22179065f5bad1be4aea80dfd69076a
Kris Kwon <ccomai92@gmail.com>
2018-09-19 01:05:42 -0700

    user done

--- app/Amazeriffic/client/users.js
+++ app/Amazeriffic/client/users.js
@@ -11,25 +11,33 @@ var main = function() {
     var $createAccount = $("<button>").text("Sign Up"); 
 
     $createAccount.on("click", function() {
-        var id = $inputId.val(); 
+        var id = {
+            "id": $inputId.val()
+        }; 
         $.post("/users", id, function(result) {
-            
+            alert("Successfully created account");
+            console.log(result); 
+            // $button.trigger("click"); 
         }).fail(function(jqXHR, textStatus, error) {
-            alert("failed" + error); 
-            $inputId.val("");
+            alert(error);
+            $inputId.val(""); 
         });
     }); 
 
     $button.on("click", function() {
         var id = $inputId.val(); 
         // var pw = $inputPw.val(); 
+        var url = "/users/" + id; 
 
-        $.get("/users/:username", id, function(result) {
+        $.get(url, id, function(result) {
             // $inputPw.val(""); 
             // console.log(result); 
-            result.load(); 
+            
+            // for now 
+            window.location.replace("http://localhost:1234"); 
+        
         }).fail(function(jqXHR, textStatus, error) {
-            alert("no user" + error);
+            alert(error);
             $inputId.val(""); 
         }); 
     });
